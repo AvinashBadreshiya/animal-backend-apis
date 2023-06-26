9e24e4fe94b5bf660d25c4f26ea7518102087498
vishv <vishv098.rejoice@gmail.com>
2023-06-26 10:23:38 +0530

    SETUP RROJECT

---
+++ database.js
@@ -0,0 +1,14 @@
+const { createSuperAdmin } = require("./Controller/superAdmin.controllers")
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const db = mongoose.connect("mongodb://localhost:27017/animalll", async (error, Db) => {
+    if (error) {
+        console.log("Database not connected", error);
+
+    } else {
+        console.log("Database Connceted");
+        await createSuperAdmin()
+    }
+});
+
+module.exports = db;
